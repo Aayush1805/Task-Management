67d7a1728b2637c2876a21db2ac6be9f3cede731
Aayush1805 <aayushvj9876@gmail.com>
2025-03-25 19:50:31 +0530

    vercel

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const Task = require('./db');
 const app = express();
-const port = 3000;
+// const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
@@ -93,6 +93,5 @@ app.delete('/tasks/:id', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+module.exports = app;
+
